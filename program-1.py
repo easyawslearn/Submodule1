c1c375582f6208ad23277f2dd9df9b1be8019743
Vijay Patel <35554905+easyawslearn@users.noreply.github.com>
2024-06-18 12:08:16 +0530

    Add files via upload

---
+++ program-1.py
@@ -0,0 +1,29 @@
+country= "India"
+name = "vijay"
+city = "Surat"
+print("Hello This is "+ country +  " from "+ name + " Located In City "+ city)
+name1="Krish"
+name2="Jeel"
+name3="Happy"
+print("Hello This is {} from {} Located In City {}".format("India","vijay","Surat"))
+print("Hello This is {} from {} Located In City {}".format(country,name,city))
+print("Hello {0} {0} {0}".format(name1,name2,name3))
+print("Hello {k} {k} {k}".format(k="Krish",j="Jeel-Jil",h="Happy"))
+print(f"Hello{name1} {name2} {name3} ")
+
+result=(100/777)
+print("Result {r:1.1f} Original {s}".format(r=result,s=result))
+list_int=[1,3,5,7,9,2,4,6,8,0]
+list_string=["One","Two","Three","Four","five"]
+list_list=[["One","two","three"],[1,2,3],[4,5,6]]
+list_list[1].reverse()
+print(list_list)
+# print(list_int)
+# print(list_string)
+# print(list_list)
+# list_string[0]="ten"
+# list_string.insert(2,"zero")
+# print(list_string)
+a=None
+print(type(a))
+print(a)
---
+++ program-2.py
@@ -0,0 +1,1 @@
+myfile = open('file.txt')
